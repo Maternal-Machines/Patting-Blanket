00a9ec0ebc0248510dba56a34459bf8093051889
Maternal-Machines <m.park@arts.ac.uk>
2026-09-14 14:31:59 +0100

    Add files via upload

---
+++ Code/Patting_Blanket/Patting_Blanket.ino
@@ -0,0 +1,126 @@
+/* Code created by Roseanne Eckheart for Dr Paulina Yurman's project: Maternal Machines: Design Speculations about Fantasies of Care.
+https://www.arts.ac.uk/colleges/central-saint-martins/research-at-csm/maternal-machines
+Designed for a blanket which pats the wearer on the back as they pat a baby doll. The user wears the blanket over there shoulder and cradles a baby doll in there arms.  
+One human pat = one pat of servo motor. 
+This code is designed to work with one servo, and a hand made pressure sensor made from layers of conductive fabric and piezoresistive material based on kobakant's designs:
+https://www.kobakant.at/DIY/?cat=26
+*/
+
+#include <Servo.h>  // include servo libary
+#include "SoftwareSerial.h"
+
+const byte PinSensor = A0;
+Servo patterServo;
+unsigned servoDownPos = 70;  // number to write to servo for it to be down.
+unsigned servoUpPos = 20;    // number to write to servo for it to be up.
+
+int PatThresh = 90;  // when sensor is triggered
+int PatHyst = 20;    // a sort of debouce
+bool held = false;   // boolean based on weather a person if continuing to hold the sensor
+
+// ---------------
+// Servo movement states
+// ---------------
+
+enum State {
+  IDLE,
+  PAT_DOWN,
+  PAT_UP
+};
+
+State state = IDLE;
+unsigned long lastChange = 0;
+
+const unsigned long interval = 200;
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  patterServo.attach(9);          // pin the servo is attached to.
+  patterServo.write(servoUpPos);  // make sure servo starts in the starting up position.
+}
+
+void loop() {
+
+  unsigned long now = millis();
+  int sensorVal = analogRead(PinSensor);
+  unsigned long now2 = millis();
+
+  // ----------------
+  // INPUT HANDLING
+  // ----------
+
+  bool triggerPat = false;  //Always set false in the loop so if it's set true at the next bit of logic it's only true for 1 frame.
+
+  // Detect NEW trigger only if not already held (this stops patting from continuing if someone is just holding the blanket or doll)
+  if (!held && sensorVal > (PatThresh + PatHyst)) {
+    held = true;
+    triggerPat = true;
+    Serial.println("TRIGGER!");
+  }
+
+  // Detect release
+  if (held && sensorVal < (PatThresh - PatHyst)) {
+    held = false;
+    Serial.println("RELEASE HELD");
+  }
+
+  // ----------------------------------------
+  // SERVO STATE MACHINE
+  // ----------------------------------------
+
+  switch (state) {
+    case IDLE:
+      patterServo.write(servoUpPos);
+
+      if (triggerPat) {  // Move to PAT_DOWN state if pat is sensed (but not if someone is just holding sensor.)
+        state = PAT_DOWN;
+        lastChange = now;
+      }
+      break;
+
+    case PAT_DOWN:
+      patterServo.write(servoDownPos);  // in PAT_DOWN state, move servo down to pat
+
+      if (now - lastChange >= interval) {  // wait for servo to get there and move to state PAT_UP
+        state = PAT_UP;
+        lastChange = now;
+      }
+
+      break;
+    case PAT_UP:
+      patterServo.write(servoUpPos);  // Move servo back to starting position (It resets the servo)
+
+
+      if (now - lastChange >= interval) {  // wait for servo to get there
+        state = IDLE;
+        lastChange = now;
+      }
+      break;
+  }
+
+  // Debug output
+  Serial.print("Sensor: ");
+  Serial.print(sensorVal);
+
+  Serial.print("  Held: ");
+  Serial.print(held);
+
+  Serial.print("  State: ");
+
+  switch (state) {
+    case IDLE:
+      Serial.println("IDLE");
+      break;
+
+    case PAT_DOWN:
+      Serial.println("PAT_DOWN");
+      break;
+
+    case PAT_UP:
+      Serial.println("PAT_UP");
+      break;
+  }
+}
